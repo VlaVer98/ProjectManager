64d81db9cd67443a256d00634c177ccd435ea739
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-09 21:14:19 +0700

    CRUD для модели Manager

---
+++ ProjectManager/Controllers/ManagerController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectManager.Models;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private ApplicationContext _db;
+        public ManagerController(ApplicationContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Managers.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Managers.Add(manager);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(manager);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Manager manager = await _db.Managers.FirstOrDefaultAsync(p => p.Id == id);
+                if (manager != null)
+                    return View(manager);
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Manager manager = await _db.Managers.FirstOrDefaultAsync(p => p.Id == id);
+                if (manager != null)
+                    return View(manager);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Manager manager)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Managers.Update(manager);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Details", new { id = manager.Id });
+            }
+
+            return View(manager);
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Manager manager = await _db.Managers.FirstOrDefaultAsync(p => p.Id == id);
+                if (manager != null)
+                    return View(manager);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Manager manager = await _db.Managers.FirstOrDefaultAsync(p => p.Id == id);
+                if (manager != null)
+                {
+                    _db.Users.Remove(manager);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
+    }
+}
--- ProjectManager/Models/User.cs
+++ ProjectManager/Models/User.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectManager.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Surname { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
         public string Patronymic { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
