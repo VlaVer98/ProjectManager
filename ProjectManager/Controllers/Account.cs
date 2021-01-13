5cb868fd4ad51e4d929b3da84e5ce023448940c4
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-13 20:38:39 +0700

    Создание Account контроллера Действие Login отвечает за логирование пользователя по email и паролю, действие Logout выполняет разлогирование.

---
+++ ProjectManager/Controllers/Account.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Models;
+using ProjectManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Controllers
+{
+    public class Account : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public Account(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = 
+                    await _signInManager.PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(loginVM.ReturnUrl) && Url.IsLocalUrl(loginVM.ReturnUrl))
+                    {
+                        return Redirect(loginVM.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Project");
+                    }
+                }
+                else
+                    ModelState.AddModelError("", "Incorrect login and (or) password");
+            }
+
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Project");
+        }
+
+    }
+}
