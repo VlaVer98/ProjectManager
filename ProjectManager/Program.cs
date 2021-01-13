e874a3c03c8107193856899257e2075b7d07039d
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-13 19:22:39 +0700

    Инициализация БД ролями и пользователями

--- ProjectManager/Program.cs
+++ ProjectManager/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProjectManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,25 @@ namespace ProjectManager
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole<int>>>();
+                    RoleInitializer.InitializeAsync(userManager, rolesManager).GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ ProjectManager/RoleInitializer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager
+{
+    public class RoleInitializer
+    {
+        public static async System.Threading.Tasks.Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            string email;
+            string password;
+
+            if (await roleManager.FindByNameAsync("supervisor") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("supervisor"));
+            }
+            if (await roleManager.FindByNameAsync("manager") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("manager"));
+            }
+            if (await roleManager.FindByNameAsync("employee") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("employee"));
+            }
+
+            email = "supervisor@project.com";
+            password = "_Aa123456";
+            if (await userManager.FindByNameAsync(email) == null)
+            {
+                Supervisor supervisor = new Supervisor { Email = email, UserName = email, Name = "NoName", Surname = "NoSurname", Patronymic = "NoPtronymic" };
+                IdentityResult result = await userManager.CreateAsync(supervisor, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(supervisor, "supervisor");
+                }
+            }
+
+            email = "manager@project.com";
+            password = "_Aa123456";
+            if (await userManager.FindByNameAsync(email) == null)
+            {
+                Manager manager = new Manager { Email = email, UserName = email, Name = "NoName", Surname = "NoSurname", Patronymic = "NoPtronymic" };
+                IdentityResult result = await userManager.CreateAsync(manager, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(manager, "manager");
+                }
+            }
+
+            email = "employee@project.com";
+            password = "_Aa123456";
+            if (await userManager.FindByNameAsync(email) == null)
+            {
+                Employee employee = new Employee { Email = email, UserName = email, Name = "NoName", Surname = "NoSurname", Patronymic = "NoPtronymic" };
+                IdentityResult result = await userManager.CreateAsync(employee, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(employee, "employee");
+                }
+            }
+        }
+    }
+}
